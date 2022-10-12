2b183ae8ae4a1e6210cb456a92988a25a7dc5c8f
Angel Baro <angelbaro2001@gmail.com>
2022-10-12 17:48:58 +0200

    color of icons changed depending if its accesible or not

--- script.js
+++ script.js
@@ -32,13 +32,29 @@ function keyTreatment(event){
 
 // nextSlide.addEventListener("click", (e) => console.log("I was clicked"));
 
+previousSlide.style.color = "gray";
+previousChapter.style.color = "gray";
 function moveChapter(amount){
     titles[chapter].style.display = "none";
     chapter += amount;
+
     if (chapter <= 0){
         chapter = 0;
+        previousChapter.style.color = "gray"; 
+        nextChapter.style.color = "black";
     } else if (chapter == chapterLenghts.length){
         chapter = chapterLenghts.length - 1;
+        nextChapter.style.color = "gray";
+    } else if(chapter == chapterLenghts.length - 1){
+        nextChapter.style.color = "gray";
+    }
+    else{
+        nextChapter.style.color = "black";
+        previousChapter.style.color = "black";
+    }
+    if(slides[chapter] == 0){
+        previousSlide.style.color = "gray";
+        nextSlide.style.color = "black";
     }
     titles[chapter].querySelector(".position").innerHTML = slides[chapter] + 1;
     titles[chapter].style.display = "inline";
@@ -51,11 +67,21 @@ function moveChapter(amount){
 
 function moveSlide(amount) {
     slides[chapter] += amount;
-    if (slides[chapter] < 0){
+    if (slides[chapter] <= 0){
         slides[chapter] = 0;
+        previousSlide.style.color = "gray";
     } else if (slides[chapter] == chapterLenghts[chapter]){
         slides[chapter] = chapterLenghts[chapter] - 1;
+        nextSlide.style.color = "gray";
     }
+    else if (slides[chapter] == chapterLenghts[chapter] - 1){
+        nextSlide.style.color = "gray";
+    }
+    else{
+        nextSlide.style.color = "black";
+        previousSlide.style.color = "black";
+    }
+    
     moveChapter(0); //WE ARE NOT MOVING THE CHAPTER, ONLY PROPAGATING THE CHANGES TO THE VIEWPORT
 }
 
